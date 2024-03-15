3f0fb24a64386bf5f40acb34be47530299e6360d
holubar <holubar@zaslat.cz>
2024-03-15 16:09:29 +0100

    update

--- src/DependencyInjection/ZaslatAuthExtension.php
+++ src/DependencyInjection/ZaslatAuthExtension.php
@@ -9,9 +9,9 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-final class ZaslatAuthBundleExtension extends Extension
+final class ZaslatAuthExtension extends Extension
 {
-    #[\Override] public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
