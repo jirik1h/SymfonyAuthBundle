ec3a623e33be08f1d1b9662fcbf310b6a3c55488
holubar <holubar@zaslat.cz>
2024-03-15 16:21:20 +0100

    update

--- src/ZaslatAuthBundle.php
+++ src/ZaslatAuthBundle.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace jirik1h\ZaslatAuthBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
+use jirik1h\ZaslatAuthBundle\DependencyInjection\ZaslatAuthExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 final class ZaslatAuthBundle extends AbstractBundle
@@ -12,5 +13,10 @@ final class ZaslatAuthBundle extends AbstractBundle
     public function getPath(): string
     {
         return dirname(__DIR__);
+    }
+
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new ZaslatAuthExtension();
     }
 }
